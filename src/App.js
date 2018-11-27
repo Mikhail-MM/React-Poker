0c319de345247260b64c8e9b9d365d9c3f681d98
Mikhail Maslyuk <leakymee@bu.edu>
2018-11-27 12:39:00 -0500

    Add general browser polyfill to index.html to debug samsung internet issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "@babel/polyfill";
+
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+import 'raf/polyfill';
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
